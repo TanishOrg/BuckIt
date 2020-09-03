e6e1d467903c8c889ec0632ea003691afc6806e0
taniya142 <taniyagehlaut14@gmail.com>
2020-09-04 00:54:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/bucketlist/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.bucketlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
