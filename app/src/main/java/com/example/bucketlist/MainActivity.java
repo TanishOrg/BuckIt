9e85357748fdef3b09d4b7d78b65bfef6bac0444
taniya142 <taniyagehlaut14@gmail.com>
2020-09-04 14:16:52 +0530

    Initial Commit

--- app/src/main/java/com/example/bucketlist/MainActivity.java
+++ app/src/main/java/com/example/bucketlist/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.bucketlist;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("BucketList");
+
+        startButton=(Button) findViewById(R.id.start);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Activity2.class);
+                startActivity(i);
+            }
+        });
 
 
 
