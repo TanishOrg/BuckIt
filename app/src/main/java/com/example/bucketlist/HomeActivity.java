bfb7f496b872ec05b003c11eb79dad8b086a2ae9
taniya142 <taniyagehlaut14@gmail.com>
2020-09-08 17:28:16 +0530

    Initial Commit

--- app/src/main/java/com/example/bucketlist/HomeActivity.java
+++ app/src/main/java/com/example/bucketlist/HomeActivity.java
@@ -3,22 +3,54 @@ package com.example.bucketlist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 import android.util.Log;
 
 import com.example.bucketlist.fragment.AddFragment;
 import com.example.bucketlist.fragment.CityFragment;
 import com.example.bucketlist.fragment.ProfileFragment;
+import com.example.bucketlist.layout.pagerAdapter;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String TAG = HomeActivity.class.getSimpleName() ;
+    private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabBar);
+        TabItem tabDreams = findViewById(R.id.tabDreams);
+        TabItem tabAchieved = findViewById(R.id.tabAchieved);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        pagerAdapter pagerAdapter = new pagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
 
         ChipNavigationBar bottomNav = findViewById(R.id.bottom_nav);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileFragment()).commit();
@@ -41,6 +73,8 @@ public class HomeActivity extends AppCompatActivity {
                 if(selectedFragment!=null){
 
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+
                 }
                 else{
                     Log.e(TAG, "Error in creating fragment" );
@@ -50,4 +84,5 @@ public class HomeActivity extends AppCompatActivity {
         });
 
     }
+
 }
