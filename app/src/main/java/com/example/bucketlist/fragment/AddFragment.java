3e9f35640fa728250cb3412d93fe6f1ed872fc66
shubham <aggarwalshubham2793@gmail.com>
2020-09-10 20:03:56 +0530

    add fragment

--- app/src/main/java/com/example/bucketlist/fragment/AddFragment.java
+++ app/src/main/java/com/example/bucketlist/fragment/AddFragment.java
@@ -10,13 +10,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.bucketlist.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AddFragment extends Fragment {
     @Nullable
     @Override
 
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        BottomNavigationView navBar = getActivity().findViewById(R.id.bottom_nav);
         return inflater.inflate(R.layout.fragment_add,container,false);
 
     }
