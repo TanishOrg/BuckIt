5c4c74bf23d97e05524e24a327bcf7c26e83b154
shubham <aggarwalshubham2793@gmail.com>
2020-09-16 22:44:53 +0530

    popup show window

--- app/src/main/java/com/example/bucketlist/DreamFragment.java
+++ app/src/main/java/com/example/bucketlist/DreamFragment.java
@@ -1,5 +1,6 @@
 package com.example.bucketlist;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.bucketlist.data.DatabaseHandler;
+
 public class DreamFragment extends Fragment {
     @Nullable
 
     RecyclerView recyclerView;
     RecyclerAdapterDream recyclerAdapterDream;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dream,container,false);
@@ -22,6 +26,8 @@ public class DreamFragment extends Fragment {
         recyclerView = view.findViewById(R.id.recyclerView);
         recyclerAdapterDream =new RecyclerAdapterDream();
         recyclerView.setAdapter(recyclerAdapterDream);
+
+
         return view;
     }
 }
--- app/src/main/java/com/example/bucketlist/RecyclerAdapterDream.java
+++ app/src/main/java/com/example/bucketlist/RecyclerAdapterDream.java
@@ -1,11 +1,14 @@
 package com.example.bucketlist;
 
+import android.app.Dialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,22 +16,35 @@ import androidx.recyclerview.widget.RecyclerView;
 public class RecyclerAdapterDream extends RecyclerView.Adapter<RecyclerAdapterDream.ViewHolder>{
 
     @NonNull
+    Dialog myDialog;
 
     private static final String TAG = "RECYCLERadapter";
     int count = 0;
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, int viewType) {
 
         Log.i(TAG,"oncreateviewholder" + count++);
-
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.row_item_dream,parent,false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        final ViewHolder viewHolder = new ViewHolder(view);
+
+        myDialog = new Dialog(parent.getContext(),android.R.style.Theme_Translucent_NoTitleBar);
+        myDialog.setContentView(R.layout.popup_show_window);
+
+        viewHolder.card_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(parent.getContext(), "test click" + String.valueOf(viewHolder.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                myDialog.show();
+            }
+        });
 
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
 
     }
 
@@ -39,6 +55,7 @@ public class RecyclerAdapterDream extends RecyclerView.Adapter<RecyclerAdapterDr
 
     class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout card_item;
         ImageView imageView;
         TextView cardTitle , cardTargetDate;
         public ViewHolder(@NonNull View itemView) {
@@ -47,6 +64,9 @@ public class RecyclerAdapterDream extends RecyclerView.Adapter<RecyclerAdapterDr
             imageView = itemView.findViewById(R.id.cardCategoryIcon);
             cardTitle = itemView.findViewById(R.id.cardTitle);
             cardTargetDate = itemView.findViewById(R.id.cardTargetDate);
+            card_item = itemView.findViewById(R.id.card_item);
+
+
         }
     }
 }
