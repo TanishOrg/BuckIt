bf1ef2773684428f4fb35fdda9274b6fa4f32dfe
taniya142 <taniyagehlaut14@gmail.com>
2020-09-29 13:40:29 +0530

    t

---
+++ app/src/main/java/com/example/bucketlist/MyAppGlideModule.java
@@ -0,0 +1,7 @@
+package com.example.bucketlist;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+public class MyAppGlideModule extends AppGlideModule {
+}
