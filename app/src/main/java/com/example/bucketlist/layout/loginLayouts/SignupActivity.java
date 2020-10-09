68a94a936da1da2bc3f852c52bbededf194706ef
shubham <aggarwalshubham2793@gmail.com>
2020-10-09 23:43:08 +0530

    login

--- app/src/main/java/com/example/bucketlist/layout/loginLayouts/SignupActivity.java
+++ app/src/main/java/com/example/bucketlist/layout/loginLayouts/SignupActivity.java
@@ -10,15 +10,25 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.bucketlist.layout.userLayout.ContactEntry;
 import com.example.bucketlist.R;
+import com.example.bucketlist.layout.userLayout.UserDetail;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignupActivity extends AppCompatActivity
         implements View.OnClickListener {
@@ -29,7 +39,9 @@ public class SignupActivity extends AppCompatActivity
     private EditText signUpPasswordEditText;
     private Button signUpButton;
     String email,password;
-
+    FirebaseFirestore firestore;
+    ProgressBar progressBar;
+    String from = "A";
     FirebaseAuth mAuth;
     FirebaseUser mUser;
 
@@ -38,13 +50,19 @@ public class SignupActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-
+        from = getIntent().getStringExtra("from activity");
+        Toast.makeText(this, from, Toast.LENGTH_SHORT).show();
         initializeUi();
+
+
+
     }
 
     private void initializeUi() {
             signUpemailEditText = findViewById(R.id.email_text_view);
             mAuth = FirebaseAuth.getInstance();
+            firestore = FirebaseFirestore.getInstance();
+            progressBar = findViewById(R.id.progressBar);
             signUpConfirmPasswordEditText = findViewById(R.id.confirm_password_text_view2);
             signUpPasswordEditText  = findViewById(R.id.password_text_view2);
             signUpButton = findViewById(R.id.signup_button);
@@ -55,9 +73,70 @@ public class SignupActivity extends AppCompatActivity
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.signup_button) {
-            doSignUp();
+           if (from.equals("OtpActivityLogin")){
+               linkWithPhone();
+           }
+           else{
+               doSignUp();
+           }
+            
         }
     }
+
+
+
+    private void linkWithPhone() {
+       progressBar.setVisibility(View.VISIBLE);
+        if (TextUtils.isEmpty(signUpemailEditText.getText().toString())
+                || TextUtils.isEmpty(signUpPasswordEditText.getText().toString().trim())
+                || TextUtils.isEmpty(signUpConfirmPasswordEditText.getText().toString().trim())) {
+
+            if(TextUtils.isEmpty(signUpemailEditText.getText().toString()))
+                signUpemailEditText.setError("Enter the email");
+
+            if(TextUtils.isEmpty(signUpPasswordEditText.getText().toString().trim()))
+                signUpPasswordEditText.setError("Enter the password ");
+
+            if(TextUtils.isEmpty(signUpConfirmPasswordEditText.getText().toString().trim()))
+                signUpConfirmPasswordEditText.setError("Enter confirm password");
+
+
+        }
+
+        else if ( signUpPasswordEditText.getText().toString().trim().length() < 8){
+            signUpPasswordEditText.setError("password length should be equal or more than 8 character");
+        }
+
+        else if(!signUpPasswordEditText.getText().toString().trim().equals(signUpConfirmPasswordEditText.getText().toString().trim())){
+
+
+            signUpConfirmPasswordEditText.setError("password does not match");
+            signUpPasswordEditText.setError("password does not match");
+        }
+        else{
+            email = signUpemailEditText.getText().toString().trim();
+            password = signUpConfirmPasswordEditText.getText().toString().trim();
+            AuthCredential credential = EmailAuthProvider.getCredential(email, password);
+            linkingWithPhone(credential);
+            progressBar.setVisibility(View.GONE);
+        }
+
+    }
+
+    private void linkingWithPhone(AuthCredential credential) {
+        mAuth.getCurrentUser().linkWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+           if (task.isSuccessful()){
+               Intent intent = new Intent(SignupActivity.this, UserDetail.class);
+               intent.putExtra("password",password);
+               startActivity(intent);
+               finish();
+           }
+            }
+        });
+    }
+
 
     private void doSignUp() {
         if (TextUtils.isEmpty(signUpemailEditText.getText().toString())
@@ -100,6 +179,7 @@ public class SignupActivity extends AppCompatActivity
                                 Toast.makeText(SignupActivity.this, "Successful", Toast.LENGTH_SHORT).show();
                                 //TODO ADD INTENT HERE
                                 startHome();
+
                             } else {
                                 Toast.makeText(SignupActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
                             }
