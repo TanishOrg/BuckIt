d96d5ac9e7c060268f38bb4060320376f95fd926
taniya142 <taniyagehlaut14@gmail.com>
2020-11-01 18:21:59 +0530

    PostActivity

--- app/src/main/java/com/example/bucketlist/fragments/homePageFragment/CityFragment.java
+++ app/src/main/java/com/example/bucketlist/fragments/homePageFragment/CityFragment.java
@@ -1,10 +1,13 @@
 package com.example.bucketlist.fragments.homePageFragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -13,6 +16,11 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -20,6 +28,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import androidx.viewpager.widget.ViewPager;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.bucketlist.AddNewCity;
 import com.example.bucketlist.AddNewPost;
 import com.example.bucketlist.R;
@@ -31,6 +42,9 @@ import com.example.bucketlist.adapters.PageAdapterTrendingCard;
 import com.example.bucketlist.model.ActivityModel;
 import com.example.bucketlist.model.CityModel;
 import com.example.bucketlist.model.TrendingCardModel;
+import com.example.bucketlist.myPost;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -39,6 +53,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.vansuita.gaussianblur.GaussianBlur;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,11 +68,24 @@ public class CityFragment extends Fragment implements View.OnClickListener {
     RecyclerView recyclerView,postRecyclerView;
     ViewPager viewPager;
     FirebaseFirestore firestore;
+    FirebaseAuth firebaseAuth;
     private PageAdapterTrendingCard pageAdapterTrendingCard;
     private List<TrendingCardModel> trendingCardModelList;
     TextView createCity;
     ImageView activity;
     TextView seemore,postMore;
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+    Context context;
+
+    String user_id;
+    String stringImageUri;
+
+    ImageView profileImage;
+    TextView profileName;
 
 
 
@@ -84,16 +112,54 @@ public class CityFragment extends Fragment implements View.OnClickListener {
         seemore = view.findViewById(R.id.seemore);
         postMore = view.findViewById(R.id.postMore);
 
+        toolbar = view.findViewById(R.id.toolBar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        drawerLayout = view.findViewById(R.id.drawer);
+        navigationView = view.findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.myPost:
+                        Intent i = new Intent(getContext(), myPost.class);
+                        startActivity(i);
+                }
+                return false;
+            }
+        });
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this.getActivity(),drawerLayout,toolbar,R.string.draw_open,R.string.drawer_closed);
+        actionBarDrawerToggle.syncState();
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.blackcolor));
+
+        View headerView = navigationView.getHeaderView(0);
+        ImageView cancelButton = headerView.findViewById(R.id.cancel_button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+            }
+        });
+
 
         firestore =FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        context = getActivity().getApplicationContext();
 
         activity.setOnClickListener(this);
         createCity.setOnClickListener(this);
         seemore.setOnClickListener(this);
-        postMore.setOnClickListener(this);
+//        postMore.setOnClickListener(this);
         recyclerView =  view.findViewById(R.id.recycler_view);
 
         postRecyclerView=view.findViewById(R.id.recycler_view_post);
+
+        profileImage = view.findViewById(R.id.profileImage1);
+        profileName = view.findViewById(R.id.profileName1);
+        user_id = firebaseAuth.getCurrentUser().getUid();
 
 
         trendingCardDataLoading();
@@ -113,6 +179,8 @@ public class CityFragment extends Fragment implements View.OnClickListener {
 
 
 
+
+//        loadData(user_id);
 
         return  view;
     }
@@ -309,6 +377,23 @@ public class CityFragment extends Fragment implements View.OnClickListener {
 
 
     }
+//    private void loadData(String user_id) {
+//        DocumentReference documentReference = firestore.collection("Users").document(user_id);
+//        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+//                if (error!=null){
+//                    Log.d("City Fragment error","Error:"+error.getMessage());
+//                }
+//                else {
+//                    profileName.setText(value.getString("Display Name"));
+//                    stringImageUri = value.getString("Image Uri");
+//                    Glide.with(context).load(stringImageUri).into(profileImage);
+//
+//                }
+//            }
+//        });
+//    }
 
 
 
