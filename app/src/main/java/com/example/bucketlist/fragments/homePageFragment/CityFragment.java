58dd74bf4af0b0f1a27c804f9e9bc8aee6edea58
taniya142 <taniyagehlaut14@gmail.com>
2020-11-03 16:48:49 +0530

    bookmark

--- app/src/main/java/com/example/bucketlist/fragments/homePageFragment/CityFragment.java
+++ app/src/main/java/com/example/bucketlist/fragments/homePageFragment/CityFragment.java
@@ -88,8 +88,6 @@ public class CityFragment extends Fragment implements View.OnClickListener {
     TextView profileName;
 
 
-
-
     int currentPage = 0;
     Timer timer;
     final long DELAY_MS = 500;
@@ -123,6 +121,8 @@ public class CityFragment extends Fragment implements View.OnClickListener {
                     case R.id.myPost:
                         Intent i = new Intent(getContext(), myPost.class);
                         startActivity(i);
+                    case R.id.bookmark:
+
                 }
                 return false;
             }
@@ -171,9 +171,6 @@ public class CityFragment extends Fragment implements View.OnClickListener {
         trendingbottomCardDataLoading();
 
        // autoScroll();
-
-
-
 
         loadData(user_id);
 
@@ -246,9 +243,6 @@ public class CityFragment extends Fragment implements View.OnClickListener {
         pageAdapterTrendingCard = new PageAdapterTrendingCard(getContext(),trendingCardModelList);
         viewPager.setAdapter(pageAdapterTrendingCard);
         viewPager.setPadding(150,0,150,0);
-
-
-
 
     }
 
@@ -388,8 +382,5 @@ public class CityFragment extends Fragment implements View.OnClickListener {
             }
         });
     }
-
-
-
 
 }
