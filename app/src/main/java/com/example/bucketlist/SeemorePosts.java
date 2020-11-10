b2848a63407356751ce57a0c0f9e02ac860dd052
shubham <aggarwalshubham2793@gmail.com>
2020-11-11 02:05:14 +0530

    notanki

--- app/src/main/java/com/example/bucketlist/SeemorePosts.java
+++ app/src/main/java/com/example/bucketlist/SeemorePosts.java
@@ -10,9 +10,15 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.app.SearchManager;
 import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.bucketlist.adapters.PostRecyclerAdapter;
 import com.example.bucketlist.fragments.homePageFragment.CityFragment;
@@ -30,49 +36,34 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SeemorePosts extends AppCompatActivity implements View.OnClickListener {
+import static com.example.bucketlist.R.id.searchView;
+
+public class SeemorePosts extends AppCompatActivity{
 
     PostRecyclerAdapter postRecyclerAdapter;
     List<ActivityModel> List;
     FirebaseFirestore firestore;
     RecyclerView postRecyclerView;
-    ImageView backButton;
-    SearchView searchView;
+    TextView title;
+    Toolbar toolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.seemore_posts_activity);
         postRecyclerView = findViewById(R.id.recyclerView);
-        backButton = findViewById(R.id.backButton);
         firestore = FirebaseFirestore.getInstance();
-        searchView = findViewById(R.id.searchView);
+        toolBar = findViewById(R.id.toolBar);
+        title = findViewById(R.id.title);
+        setSupportActionBar(toolBar);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
         PostLoading();
-        searchView.getBackground();
-        backButton.setOnClickListener(this);
-        searchView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (searchView.isIconified()) {
-                    searchView.setBackgroundColor(Color.WHITE);
-                }
-
-            }
-        });
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                postRecyclerAdapter.getFilter().filter(newText);
-                Log.d("TAG", "onQueryTextChange: ");
-                return false;
-            }
-        });
 
     }
 
@@ -112,11 +103,27 @@ public class SeemorePosts extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId()==R.id.backButton){
-            finish();
 
-        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.see_more_cities_menu,menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    postRecyclerAdapter.getFilter().filter(newText);
+                    Log.d("TAG", "onQueryTextChange: ");
+                    return false;
+                }
+            });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
